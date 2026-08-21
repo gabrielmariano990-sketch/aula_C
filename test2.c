dca0b934036c43a919608f4c81e4939c7079b948
GABRIEL MARIANO JORGE <gabriel.jorge@cs.up.edu.br>
2026-08-20 22:09:01 -0300

    Adiciona exercicios em C

---
+++ test2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("========================================\n");
+    printf("\tDATACENTER UP - RELATORIO DE STATUS\n");
+    printf("========================================\n");
+    printf("HOSTNAME:\tsrv-datacenter-01\n");
+    printf("UPTIME:\t\t99.98%%\n");
+    printf("STATUS CPU:\tOK (32%% uso)\n");
+    printf("MEMORIA RAM:\tOK (12.4 GB / 64 GB)\n");
+    printf("DISCO:\t\tOK (450 GB / 1 TB)\n");
+    printf("========================================\n");
+
+    return 0;
+}
